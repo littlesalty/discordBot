7379cac9fc3d48477dbb381ad80a8d357433df02
ascariu <26717750+ascariu@users.noreply.github.com>
2021-11-28 14:29:56 +0100

    refactor of config file

--- src/config.ts
+++ src/config.ts
@@ -19,30 +19,24 @@ if (result.error) {
     throw result.error
 }
 
-const unsafeEnvs = {
-    DISCORD_BOT_KEY: result?.parsed?.DISCORD_BOT_KEY,
-    DISCORD_BOT_CLIENT_ID: result?.parsed?.DISCORD_BOT_CLIENT_ID,
-    DISCORD_DEFAULT_GUILD_ID: result?.parsed?.DISCORD_DEFAULT_GUILD_ID,
-    TWITCH_CLIENT_ID: result?.parsed?.TWITCH_CLIENT_ID,
-    TWITCH_API_SECRET: result?.parsed?.TWITCH_API_SECRET,
-    BROADCASTER_ID: result?.parsed?.BROADCASTER_ID
-} as UnsafeEnvKeys
-
-
+const unsafeEnvs = result?.parsed as UnsafeEnvKeys
 
 export const env = checkForMissingEnvVars(unsafeEnvs)
 
-function checkForMissingEnvVars(envVars: UnsafeEnvKeys): EnvKeys {
+function checkForMissingEnvVars(envVars: UnsafeEnvKeys | undefined): EnvKeys {
+    const invalidArguments = (errorMessage?: string): never => { throw new Error(`Some env vars are missing: ${errorMessage}`) }
+    const safeEnvVars = envVars ?? invalidArguments()
+
     const missingEnvVars: Array<string> = []
-    Object.keys(envVars).some((envVarKey, index, array) => {
-        const envVar = envVars[envVarKey as keyof UnsafeEnvKeys]
+    Object.keys(safeEnvVars).forEach((envVarKey, index, array) => {
+        const envVar = safeEnvVars[envVarKey as keyof UnsafeEnvKeys]
         if (!envVar) {
             console.error(`${envVarKey} is not set as an environment variable`)
             missingEnvVars.push(envVarKey)
         }
 
         if (index == array.length - 1 && missingEnvVars.length > 0) {
-            throw new Error(`Some env vars are missing: ${missingEnvVars}`)
+            invalidArguments(missingEnvVars.join(", "))
         }
     })
 
