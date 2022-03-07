5ee361f59f8d044c6bbeb4bab39114ddce7788ad
ascariu <26717750+ascariu@users.noreply.github.com>
2022-03-07 23:30:17 +0100

    format no-spam

--- src/utils/no-spam.ts
+++ src/utils/no-spam.ts
@@ -1,5 +1,4 @@
-import { User } from "discord.js";
-
+import { User } from "discord.js"
 
 const minutes = (number: number) => number * 60 * 1000
 
@@ -7,21 +6,22 @@ const TIMEOUT_BETWEEN_REPLIES = minutes(5)
 let lastUser: User | null = null
 let timeoutHandle: number = -1
 
-
 const canWeReplyTo = ({ id }: User) => lastUser?.id != id
 const scheduleClearUserTimeout = () => {
-    if (timeoutHandle !== -1) {
-        clearTimeout(timeoutHandle)
-    }
-    timeoutHandle = setTimeout(() => (lastUser = null), TIMEOUT_BETWEEN_REPLIES) as unknown as number
+  if (timeoutHandle !== -1) {
+    clearTimeout(timeoutHandle)
+  }
+  timeoutHandle = setTimeout(
+    () => (lastUser = null),
+    TIMEOUT_BETWEEN_REPLIES
+  ) as unknown as number
 }
 
 export const doIfCanReply = <T>(user: User, action: () => T) => {
+  if (canWeReplyTo(user)) {
+    scheduleClearUserTimeout()
+    return action()
+  }
 
-    if (canWeReplyTo(user)) {
-        scheduleClearUserTimeout()
-        return action()
-    }
-
-    return null
-} 
+  return null
+}
