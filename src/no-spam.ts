fc43384bd84c8260f80387ed84d2b609d80bca0a
Miquel Àngel Román <miquelrc95@outlook.es>
2022-03-07 23:08:41 +0100

    Adds utilities to achieve no-spam from the same user

---
+++ src/no-spam.ts
@@ -0,0 +1,27 @@
+import { User } from "discord.js";
+
+
+const minutes = (number: number) => number*60*1000
+
+const TIMEOUT_BETWEEN_REPLIES = minutes(5)
+let lastUser: User|null = null
+let timeoutHandle: number = -1
+
+
+const canWeReplyTo = ({id}:User) => lastUser?.id != id
+const scheduleClearUserTimeout = () => {
+  if(timeoutHandle !== -1) {
+    clearTimeout(timeoutHandle)
+  }
+  timeoutHandle = setTimeout(() => (lastUser = null), TIMEOUT_BETWEEN_REPLIES) as unknown as number
+}
+
+export const doIfCanReply = <T> (user: User, action: () => T) => {
+  
+  if (canWeReplyTo(user)) {
+    scheduleClearUserTimeout()
+    return action()
+  }
+
+  return null
+}
